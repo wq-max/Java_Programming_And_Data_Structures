c86c79a3f7e4781014cb326378c1040b91806779
wq-max <53320735+wq-max@users.noreply.github.com>
2022-02-26 11:42:11 +0800

    chapter19 end

---
+++ chapter19/src/geometrics/Rectangle.java
@@ -0,0 +1,47 @@
+package geometrics;
+
+public class Rectangle extends GeometricObject{
+
+    private double width;
+    private double height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double height) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}

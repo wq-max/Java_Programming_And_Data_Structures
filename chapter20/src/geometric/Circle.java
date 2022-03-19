b06a694d4fc8552803b7208ce0ee5a649378810b
wq-max <53320735+wq-max@users.noreply.github.com>
2022-03-19 17:01:04 +0800

    chapter20 end

---
+++ chapter20/src/geometric/Circle.java
@@ -0,0 +1,45 @@
+package geometric;
+
+public class Circle extends GeometricObject{
+    private double radius;
+
+    public Circle(){
+
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled){
+        this.radius = radius;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+
+    @Override
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getDiameter(){
+        return 2 * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    public void printCircle(){
+        System.out.println("The circle is created " + getDateCreated() + " and the radius is " + radius);
+    }
+}

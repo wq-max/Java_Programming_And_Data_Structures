fd32e3d23617d1c0071496b73ab34f2e5014060a
QiQi <2550178538@qq.com>
2022-04-11 11:26:13 +0800

    chapter21 end

---
+++ chapter21/src/map/CountOccurrenceOfWords.java
@@ -0,0 +1,30 @@
+package map;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CountOccurrenceOfWords {
+
+    public static void main(String[] args) {
+        String text = "Good morning. Have a good class. " +
+                "Have a good visit. Have fun!";
+
+        Map<String, Integer> map = new TreeMap<>();
+
+        String[] words = text.split("[\\s+\\p{P}]");
+        for (String word : words) {
+            String key = word.toLowerCase();
+            if (key.length() > 0) {
+                if (!map.containsKey(key)) {
+                    map.put(key, 1);
+                } else {
+                    int value = map.get(key);
+                    value++;
+                    map.put(key, value);
+                }
+            }
+        }
+        map.forEach((k, v) -> System.out.println(k + "\t" + v));
+
+    }
+}
